04d8a76fc638d83c0e3d8ee4d4a77f4f16459f16
shin-ga-eun <sge7102@naver.com>
2020-07-08 22:02:38 +0900

    add posts 등록.

---
+++ src/main/java/com/shingaeun/board/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.shingaeun.board.service.posts;
+
+import com.shingaeun.board.domain.posts.PostsRepository;
+import com.shingaeun.board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
