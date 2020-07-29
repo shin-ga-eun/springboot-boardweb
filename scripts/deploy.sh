23c7deed37a6e1e1a8b8db5cfd3416201aa97df9
shin-ga-eun <sge7102@naver.com>
2020-07-29 17:40:07 +0900

    자동화 오류.
    문제 현재 pid를 찾지 못한다. -> deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,21 +9,21 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-#CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME*} | grep jar | awk '{print $1}')
 
-CURRENT_PID=$(pgrep -f $PROJECT_NAME*.jar)
+#CURRENT_PID=$(pgrep -f $PROJECT_NAME.*.jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-#
-#if [ -f "$CURRENT_PID" ]; then
-#        echo "> 현재 실 중인 애플리케이션이 없으므로 종료하지 않습니다."
-#else
-#        echo "> kill -15 $CURRENT_PID"
-#        kill -15 $CURRENT_PID
-#        sleep 5
-#fi
 
-kill -15 $CURRENT_PID
+if [ -f "$CURRENT_PID" ]; then
+        echo "> 현재 실 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 "$CURRENT_PID"
+        sleep 5
+fi
+
+#kill -15 $CURRENT_PID
 
 echo ">pid 죽임"
 
