965df6d37c06fcb748f073505881495b53a7e96e
shin-ga-eun <sge7102@naver.com>
2020-07-29 18:33:58 +0900

    테스트.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,19 +7,23 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+#echo "> 현재 구동 중인 애플리케이션 pid 확인"
+#
+#CURRENT_PID=$(pgrep -fl springboot-boardweb | grep jar | awk '{print $1}')
+#
+#echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID 이다."
+#
+#if [ -z "$CURRENT_PID" ]; then
+#        echo "> 현재 수동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#        echo "> kill -15 $CURRENT_PID"
+#        kill -15 $CURRENT_PID
+#        sleep 5
+#fi
 
-CURRENT_PID=$(pgrep -fl springboot-boardweb | grep jar | awk '{print $1}')
+fuser -k -n tcp 8080
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID 이다."
-
-if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 수동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
-fi
+echo "> 8080포트 죽이기를 사용하고 있는 pid 죽이기"
 
 echo "> 새 애플리케이션 배포"
 
