c4ede878c3b8bd4ecce9e77c2c56813732538ee5
shin-ga-eun <sge7102@naver.com>
2020-07-31 15:51:41 +0900

    profileController 추가.
    배포 시 8081 쓸지, 8082 쓸지

---
+++ src/main/java/com/shingaeun/board/web/ProfileController.java
@@ -0,0 +1,32 @@
+package com.shingaeun.board.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    /*
+    env.getActiveProfiles()
+    현재 실행중인 ActiveProfile을 모두 가져온다.
+     */
+    @GetMapping("/profile")
+    public String profile(){
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+
+}
